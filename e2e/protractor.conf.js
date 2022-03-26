cd0aecaa028fd2c007f3c93a01a882648bd56a98
Maria Ramahalison <mariaramahalison@gmail.com>
2022-03-26 19:17:03 +0300

    port

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
     browserName: 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'https://maria-test-mean.herokuapp.com:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
